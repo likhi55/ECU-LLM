0e7fc7c14a5062362cd8619ecff083ec01b94106
LIKHITH HN <2023aa05332@wilp.bits-pilani.ac.in>
2025-08-13 18:20:31 +0530

    SCR000002: implement accelerator pedal logic

--- app/h_files/ecu.h
+++ app/h_files/ecu.h
@@ -1,4 +1,14 @@
 #ifndef ECU_H
 #define ECU_H
+
+// SCR1
 int compute_engine_state(int ignition_switch);
+
+// SCR2
+int parse_max_engine_speed(const char *calib_path, int fallback_rpm);
+int update_engine_speed(int engine_state,
+                        int acc_pedal_position_deg,
+                        int prev_engine_speed_rpm,
+                        int max_engine_speed_rpm);
+
 #endif
